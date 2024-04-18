b220f69a26b46d9587ac0732b4d0f6f3ca13ddc0
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-18 09:02:26 +0430

    id sets

--- script.js
+++ script.js
@@ -90,8 +90,14 @@ products.forEach(
     dessertCards.innerHTML += `
       <div class="dessert-card">
         <h2>${name}</h2>
-
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
       </div>
     `;
   }
 );
+
